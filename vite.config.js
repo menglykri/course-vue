494a078b2f91fbf34299448a8f6e8ea9bd7c2dcb
menglykri <menglykri9@gmail.com>
2026-06-02 09:28:36 +0700

    git vite

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ],
-  base: '/course-vue/',
+  plugins: [vue(), tailwindcss()],
+  base: '/course_vue/',
 })
