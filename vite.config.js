1ba9022045fbca8485656f639927339422932f7b
menglykri <menglykri9@gmail.com>
2026-06-02 09:31:13 +0700

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/course_vue/',
+  base: '/course-vue/',
 })
